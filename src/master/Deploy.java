82b94bae97f6111b9606508f72ba6865b51035b9
Axel Camara <axel.camara@hotmail.fr>
2019-10-21 15:15:58 +0200

    Fix bad comprehension 10-1

--- src/master/Deploy.java
+++ src/master/Deploy.java
@@ -58,14 +58,16 @@ public class Deploy {
         ArrayList<String> hostnames = Deploy.read_file(path_to_hostnames);
         ArrayList<String> health_checks = new ArrayList<>();
         ArrayList<String> check_dir = new ArrayList<>();
+        ArrayList<String> create_dirs = new ArrayList<>();
         ArrayList<String> run_copy = new ArrayList<>();
 
         // Create list of commands for each machines
         assert hostnames != null;
         for (String hostname : hostnames) {
             health_checks.add("ssh -o StrictHostKeyChecking=no acamara@" + hostname + " hostname");
-            run_copy.add("scp -pr " + host_path + " acamara@" + hostname + ":" + remote_path);
-            check_dir.add("ssh acamara@" + hostname + " ls " + remote_path);
+            create_dirs.add("ssh acamara@" + hostname + " if test ! -d " + remote_path + "; then mkdir -p " + remote_path + "; fi");
+            check_dir.add("ssh acamara@" + hostname + " ls /tmp/acamara");
+            run_copy.add("scp -r " + host_path + "acamara@" + hostname + ":" + remote_path);
         }
 
         // Apply health checker
@@ -74,16 +76,15 @@ public class Deploy {
         // Check all machine are alive and deploy jar
         boolean isNodesOk = returnValue.stream().allMatch(x -> x);
         if (isNodesOk) {
-            launch_actions_without_return(run_copy);
+            launch_actions_without_return(create_dirs);
 
             // wait for directories creation and check the creation
             Thread.sleep(3000);
             returnValue = launch_actions_with_return(check_dir);
-            System.out.println(returnValue);
 
             // if all directories are created, deploy jar file
             if (returnValue.stream().allMatch(x -> x)) {
-                return true;
+                launch_actions_without_return(run_copy);
             } else {
                 System.out.println("Something went wrong during the directories creation");
                 return false;
@@ -92,6 +93,7 @@ public class Deploy {
             System.out.println("Not all nodes are safe, please check connection");
             return false;
         }
+        return true;
     }
 
     public static void main(String[] args) throws InterruptedException {
--- src/master/Master.java
+++ src/master/Master.java
@@ -4,9 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Master {
 
@@ -92,11 +97,55 @@ public class Master {
         }
     }
 
+    public static List<String> list_directory(String path_to_directory){
+        try (Stream<Path> walk = Files.walk(Paths.get(path_to_directory))) {
+
+            return walk.filter(Files::isRegularFile)
+                    .map(Path::toString).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
-        Boolean return_value = Deploy.deploy("data/hostnames.txt", "/tmp/acamara/splits",
-                "/tmp/acamara/");
-        System.out.println(return_value);
+        ArrayList<String> hostnames = Deploy.read_file(args[0]);
+        List<String> files = list_directory(args[1]);
+        ArrayList<String> health_checks = new ArrayList<>();
+        ArrayList<String> create_dirs = new ArrayList<>();
+        ArrayList<String> check_dir = new ArrayList<>();
+        ArrayList<String> run_copy = new ArrayList<>();
+        Random rand = new Random();
+
+        for (String file:files){
+            String slave = hostnames.get(rand.nextInt(hostnames.size()));
+            health_checks.add("ssh -o StrictHostKeyChecking=no acamara@" + slave + " hostname");
+            create_dirs.add("ssh acamara@" + slave + " if test ! -d " + args[2] + "; then mkdir -p " + args[2] + "; fi");
+            run_copy.add("scp -r " + file + " acamara@" + slave + ":" + args[2]);
+            check_dir.add("ssh acamara@" + slave + " ls " + args[2]);
+        }
+        // Apply health checker
+        List<Boolean> returnValue = Deploy.launch_actions_with_return(health_checks);
+
+        // Check all machine are alive and deploy jar
+        boolean isNodesOk = returnValue.stream().allMatch(x -> x);
+        if (isNodesOk) {
+            Deploy.launch_actions_without_return(create_dirs);
+
+            // wait for directories creation and check the creation
+            Thread.sleep(3000);
+            returnValue = Deploy.launch_actions_with_return(check_dir);
+
+            // if all directories are created, deploy jar file
+            if (returnValue.stream().allMatch(x -> x)) {
+                Deploy.launch_actions_without_return(run_copy);
+            } else {
+                System.out.println("Something went wrong during the directories creation");
+            }
+        } else {
+            System.out.println("Not all nodes are safe, please check connection");
+        }
     }
 }
 
